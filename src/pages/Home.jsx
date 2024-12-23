863122d1f6d26ce6624b4ee1e203f3968d04363d
dubeyrishabh123 <DUBEYVISHAL690@GMAIL.COM>
2024-12-24 02:45:06 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+import { assets } from '../assets/assets'
+
+const home = () => {
+  return (
+    <div>
+       <Header/>
+    </div>
+  )
+}
+
+export default home
