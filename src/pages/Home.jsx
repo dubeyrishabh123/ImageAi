8ed0aa441c1aaf919b21dee44a4f516c3b350f85
Rishabh Dubey <DUBEYVISHAL690@GMAIL.COM>
2024-12-29 01:01:43 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Header from '../components/Header'
 import { assets } from '../assets/assets'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
+import GenerateBtn from '../components/GenerateBtn'
 
 const home = () => {
   return (
     <div>
        <Header/>
+       <Steps/>
+       <Description/>
+       <Testimonials/>
+       <GenerateBtn/>
     </div>
   )
 }
